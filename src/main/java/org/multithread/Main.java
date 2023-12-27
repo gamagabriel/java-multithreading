b36d640e3e10447bff2251562d035608c928879d
gamagabriel <gbr.gama99@gmail.com>
2023-12-26 23:28:33 -0300

    Initial commit

--- src/main/java/org/multithread/Main.java
+++ src/main/java/org/multithread/Main.java
@@ -2,6 +2,6 @@ package org.multithread;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Project created to learning about multithreading.");
+        System.out.println("Hello world, project created to learning about multithreading.");
     }
 }
